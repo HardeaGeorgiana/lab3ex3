d27d187ed8235cac343154c6461767ad8d750e15
Tenie Alexandru <alexandru_ale26@yahoo.com>
2022-12-13 22:41:54 +0200

    Add project files.

---
+++ lab3ex3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace lab3ex3
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti un numar");
+            int number = int.Parse(Console.ReadLine());
+            PatratPerfect(number);
+
+            static void PatratPerfect(int number)
+            {
+                if (number < 1)
+                {
+                    Console.WriteLine("Introduceti un numar mai mare decat 0");
+                    return;
+                }
+
+                double num = Math.Sqrt(number); 
+
+                if(num == (int)num)
+                {
+                    Console.WriteLine($"Numarul {number} este patrat perfect");
+                }
+                else
+                {
+                    Console.WriteLine($"Numarul {number} nu este patrat perfect");
+                }
+            }
+        }
+    }
+}
